dfe5dcf7afd8f602c5e6145ce44375e9054c81c2
Alesha Reed <aleshak83@gmail.com>
2020-04-04 18:17:00 -0500

    Add project files.

---
+++ FamilyDictionary/Family.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyDictionary
+{
+    class Family
+    {
+        public string Relation { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ FamilyDictionary/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FamilyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Family family1 = new Family()
+            {
+                Relation = "Sister",
+                Name = "Adrienne",
+                Age = 40
+            };
+
+            Family family2 = new Family()
+            {
+                Relation = "Brother",
+                Name = "Johnathan",
+                Age = 42
+            };
+            Family family3 = new Family()
+            {
+                Relation = "GodSister",
+                Name = "Donna",
+                Age = 41
+            };
+
+
+            Dictionary <string, Family> myFamily = new Dictionary<string, Family>();
+
+            myFamily.Add(family1.Relation, family1);
+            myFamily.Add(family2.Relation, family2);
+            myFamily.Add(family3.Relation, family3);
+
+            Family mySister = myFamily["Sister"];
+            Console.WriteLine($"I love {mySister.Name}");
+
+            foreach (KeyValuePair<string, Family> familyKeyValuePair in myFamily)
+            {
+                Family familyVal = familyKeyValuePair.Value;
+                Console.WriteLine($"I love my {familyVal.Relation} {familyVal.Name}.\n {familyVal.Name} is  the best {familyVal.Age} year old I know!");
+            }
+
+
+        }
+    }
+}
